f479f23c24bc0bead244f0c2fdd1c4af99014108
Anoushka Raghuveeran <anoushkaraghuveeran@gmail.com>
2025-04-09 13:42:02 +0530

    first commit

---
+++ bgchanger.js
@@ -0,0 +1,19 @@
+document.getElementById("red").addEventListener("click", function () {
+  document.body.style.backgroundColor = "red";
+});
+
+document.getElementById("green").addEventListener("click", function () {
+  document.body.style.backgroundColor = "green";
+});
+
+document.getElementById("blue").addEventListener("click", function () {
+  document.body.style.backgroundColor = "blue";
+});
+
+document.getElementById("yellow").addEventListener("click", function () {
+  document.body.style.backgroundColor = "yellow";
+});
+
+document.getElementById("purple").addEventListener("click", function () {
+  document.body.style.backgroundColor = "purple";
+});
